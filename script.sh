6b082bac67e746ffb461a5d717793b72b9229961
khinthuzar-joe <133834108+khinthuzar-joe@users.noreply.github.com>
2023-09-13 06:00:57 +0630

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+plain='\033[0m'
+
+red='\e[31m'
+yellow='\e[33m'
+gray='\e[90m'
+green='\e[92m'
+blue='\e[94m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+# Font Size
+# h6 = SMALL , h4 = AVERAGE , h3 = LARGE
+# Check Root
+[[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} You must use root user to run this script!\n" && exit 1
+
+sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
+echo "
+<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+</font></h3>
+<h3><font color='magenta'>--- ۩ PREMIUM SERVER ۩ ---
+</font></h3>
+
+<h3><font color='green'>--- ۩ SERVER BY YOE ۩ ---
+</font></h3>
+
+<h4><font color='cyan'>Telegram >> https://t.me/Yoeyar007
+</font></h4>
+
+<h4><font color='blue'>Telegram >> #Crd404
+</font></h4>
+
+<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+</font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
+useradd "yoe" --shell=/bin/false -M
+echo "yoe:yoe" | chpasswd
+
+echo ""
+
+echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+echo -e "${cyan} ___   ___          ________          ___   ___                               ${plain}"
+echo -e "${cyan}|\  \ |\  \        |\   __  \        |\  \ |\  \                              ${plain}"
+echo -e "${cyan}\ \  \|_\  \       \ \  \|\  \       \ \  \|_\  \                             ${plain}"
+echo -e "${cyan} \ \______  \       \ \  \/\  \       \ \______  \                            ${plain}"
+echo -e "${cyan}  \|_____|\  \       \ \  \/\  \       \|_____|\  \                           ${plain}"
+echo -e "${cyan}         \ \__\       \ \_______\             \ \__\                          ${plain}"
+echo -e "${cyan}          \|__|        \|_______|              \|__|                          ${plain}"
+echo -e "${green}Contact the developer https://t.me/nkka404 for more information              ${plain}"
+echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+
+echo -e "${cyan}i am Yoe 😎 ${plain}"
